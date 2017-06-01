3995bb1eedf7f59e275f0553928b184039d87f33
Radek Starosta <radek.starosta@gmail.com>
2017-06-01 22:46:53 +0200

    Ignore parameters and headers.

--- src/main/java/ass/starorad/semestralproject/server/impl/ParsedHttpRequest.java
+++ src/main/java/ass/starorad/semestralproject/server/impl/ParsedHttpRequest.java
@@ -2,16 +2,20 @@ package ass.starorad.semestralproject.server.impl;
 
 import ass.starorad.semestralproject.server.IHttpRequest;
 import io.netty.handler.codec.http.HttpRequest;
+import java.io.UnsupportedEncodingException;
 import java.nio.channels.SocketChannel;
+import java.util.Base64;
 
 public class ParsedHttpRequest implements IHttpRequest {
 
   private SocketChannel client;
   private HttpRequest httpRequest;
+  private String path;
 
   public ParsedHttpRequest(SocketChannel client, HttpRequest httpRequest) {
     this.client = client;
     this.httpRequest = httpRequest;
+    this.path = extractPathFromRequest(httpRequest);
   }
 
   public SocketChannel getClient() {
@@ -23,10 +27,26 @@ public class ParsedHttpRequest implements IHttpRequest {
     return httpRequest;
   }
 
-  //TODO: Save this into a property?
   @Override
   public String getPath() {
-    String path = httpRequest.uri().substring(1);
+    return path;
+  }
+
+  private String extractPathFromRequest(HttpRequest request) {
+    String path = request.uri();
+
+    int paramIndex = path.indexOf('?');
+    int anchorIndex = path.indexOf('#');
+
+    if (paramIndex != -1) { // params present, must be first
+      path = path.substring(1, paramIndex);
+    } else {
+      if (anchorIndex != -1) { // only anchor
+        path = path.substring(1, anchorIndex);
+      } else { // nothing
+        path = path.substring(1);
+      }
+    }
 
     return path.isEmpty() ? "index.html" : path;
   }
